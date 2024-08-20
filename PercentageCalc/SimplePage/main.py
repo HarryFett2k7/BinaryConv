eba1c30572b08061a1f221a7df065327fdb13a0b
HarryFett2k7 <95986296+HarryFett2k7@users.noreply.github.com>
2024-08-20 11:42:50 +0100

    Update main.py

--- PercentageCalc/SimplePage/main.py
+++ PercentageCalc/SimplePage/main.py
@@ -1,31 +1,21 @@
 import streamlit as st
-from PIL import Image
 
-st.header("Percentage calculator", divider="rainbow" )
-st.subheader("A simple tool to calculate percentage change.")
+st.header("Binary Convertor", divider="rainbow" )
+st.subheader("A simple tool to convert numbers to and from binary.")
 
 st.markdown('''
     This is a test while the website is under development
 ''')
 
-increase = st.toggle("Increase")
-decrease = st.toggle("Decrease")
+itob = st.toggle("Integer to binary")
+btoi = st.toggle("Binary to integer")
 
-if increase:
-    start_entered = st.number_input("Enter start number: ", value = 0.01)
-    final_entered = st.number_input("Enter final number: ", value = 0.01)
-    def calc(pStart, pFinal):
-        return (pFinal - pStart) / pStart * 100
-    output = calc(start_entered, final_entered)
-    st.markdown("+" + str(output) + "%")
-    
+if itob:
+    number = st.text_input("Enter your number: ", value = 1)
+    binary = bin(int(number))
+    st.markdown("Binary number: " + str(binary[2:]))
 
-if decrease:
-    start_entered1 = st.number_input("Enter start number: ", value = 0.01)
-    final_entered1 = st.number_input("Enter final number: ", value = 0.01)
-    def calc(pStart, pFinal):
-        return (pStart - pFinal) / pStart * 100
-    output1 = calc(start_entered1, final_entered1)
-    st.markdown("-" + str(output1) + "%")
-
-st.link_button("Youtube", "https://www.youtube.com")
+if btoi:
+    number = st.text_input("Enter binary value: ", value = 1)
+    integer = int(number, 2)
+    st.markdown("Integer is: " + str(integer))
